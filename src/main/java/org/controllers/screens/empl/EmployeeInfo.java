d639dd323929a0fb9629ed42928df6c804d7c489
Denis <volkov.denis.mt190606@gmail.com>
2021-01-20 00:39:08 +0500

    listEmp

--- src/main/java/org/controllers/screens/empl/EmployeeInfo.java
+++ src/main/java/org/controllers/screens/empl/EmployeeInfo.java
@@ -2,6 +2,8 @@ package org.controllers.screens.empl;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.Objects;
+
 public class EmployeeInfo {
 
      SimpleStringProperty personnelNumber;
@@ -17,5 +19,23 @@ public class EmployeeInfo {
         this.id = new SimpleStringProperty(id);
         this.division = new SimpleStringProperty(division);
         this.position = new SimpleStringProperty(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeInfo that = (EmployeeInfo) o;
+        return fio.get().equals(that.fio.get());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fio);
+    }
+
+    @Override
+    public String toString() {
+        return fio.get();
     }
 }
--- src/main/java/org/controllers/screens/empl/EmployeesListController.java
+++ src/main/java/org/controllers/screens/empl/EmployeesListController.java
@@ -1,5 +1,7 @@
 package org.controllers.screens.empl;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,6 +11,7 @@ import javafx.scene.control.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeesListController {
     //table start
@@ -26,7 +29,7 @@ public class EmployeesListController {
     @FXML
     private TextField fioTextField;
     @FXML
-    private ComboBox<String> fioComboBox;
+    private ComboBox<EmployeeInfo> fioComboBox;
     @FXML
     private TableView<EmployeeInfo> employeesTableView;
     @FXML
@@ -35,7 +38,7 @@ public class EmployeesListController {
     @FXML
     private Pagination pageScrollPagination;
 
-    private ObservableList<String> options;
+    //private ObservableList<String> options;
     private List<EmployeeInfo> employeesList;
     private Integer rowsPerPage = 20;
     private static int totalPages;
@@ -43,16 +46,56 @@ public class EmployeesListController {
     @FXML
     private void initialize() {
         employeesList = new ArrayList<>();
-        for (int i = 0; i < 300; i++) {
-            employeesList.add(new EmployeeInfo(String.valueOf(i+1),"Волков Василий Егорович","465789123","Новое","Дежурный"));
-        }//
+        employeesList.add(new EmployeeInfo(String.valueOf(1),"Волков Василий Егорович","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(2),"Ардаков Игорь Герасимович","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(3),"Донченко Иван Андреевич","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(4),"Кулагина Юлия Анатольевна","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(5),"Бирюков Евгений Евгеньевич","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(6),"Гришина Ольга Константиновна","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(7),"Карсева Полина Алексеевна","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(8),"Логинов Сергей Николаевич","465789123","Новое","Дежурный"));
+        employeesList.add(new EmployeeInfo(String.valueOf(9),"Жаркова Алла Юрьевна","465789123","Новое","Дежурный"));
+        for (int i = 10; i < 100; i++) {
+            if(i == 66) employeesList.add(new EmployeeInfo(String.valueOf(i),"Зебра Юлия Анатольевна","465789123","Новое","Дежурный"));
+            else employeesList.add(new EmployeeInfo(String.valueOf(i),"Жаркова Алла Юрьевна","465789123","Новое","Дежурный"));
+        }
+
+        //set field fio and combo box
+        fioTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            //System.out.println("textfield changed from " + oldValue + " to " + newValue);
+            fioComboBox.setItems(filterOnTypingFio(newValue));
+            //options = filterOnTypingFio(newValue);
+            fioComboBox.show();
+        });
+        fioComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) return;
+            fioTextField.setText(newValue.fio.get());
+            int indexRow =  employeesList.indexOf(newValue) + 1;
+            System.out.println(indexRow);
+            System.out.println("getIndexRowOnPage(newValue)"+getIndexRowOnPage(newValue));
+            //employeesTableView.getSelectionModel().select(indexRow);
+            //employeesTableView.scrollTo(indexRow);
+            System.out.println(getIndexPageOnFio(newValue));
+            pageScrollPagination.setCurrentPageIndex(getIndexPageOnFio(newValue));
+            Platform.runLater(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    employeesTableView.requestFocus();
+                    employeesTableView.getSelectionModel().select(getIndexRowOnPage(newValue));
+                    employeesTableView.getFocusModel().focus(getIndexRowOnPage(newValue));
+                }
+            });
+
+        });
+
         //set page on list 20, 30, 50
         SpinnerValueFactory<Integer> valueFactory = //
                 new SpinnerValueFactory.ListSpinnerValueFactory<Integer>( FXCollections.observableArrayList( Arrays.asList(20,30,50)));
         paginatorSpinner.setValueFactory(valueFactory);
         //add listener to spinner page
         paginatorSpinner.valueProperty().addListener((obs, oldValue, newValue) -> {
-            System.out.println("New value: "+newValue);
             switch (newValue) {
                 case 20 : {
                     rowsPerPage = 20;
@@ -93,22 +136,30 @@ public class EmployeesListController {
 
         pageScrollPagination.setPageFactory(this::createPage);
 
-    options = FXCollections.observableArrayList("Option 1", "Option 2", "Option 3","Option 4", "Option 5", "Option 6","Option 7", "Option 8", "Option 9","Option 10", "Option 11", "Option 12");
-    fioComboBox.setItems(options);
-        fioTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("textfield changed from " + oldValue + " to " + newValue);
-            fioComboBox.show();
-        });
+
+    }
+    private ObservableList<EmployeeInfo> filterOnTypingFio(String pattern) {
+        List<EmployeeInfo> resultFiltering = employeesList.stream()
+                .filter(employeeInfo -> employeeInfo.fio.get().matches(".*"+pattern+".*")).collect(Collectors.toList());
+        System.out.println(resultFiltering);
+        return FXCollections.observableArrayList(resultFiltering);
+    }
+    private int getIndexPageOnFio(EmployeeInfo employeeInfo) {
+        int i = employeesTableView.getItems().indexOf(employeeInfo) + 1;
+        int res = (int) Math.ceil(i / (float) rowsPerPage) - 1;
+        if(res<0) throw new RuntimeException("page -1 ");
+        return res;
+    }
+    private int getIndexRowOnPage(EmployeeInfo employeeInfo) {
+        int i = employeesTableView.getItems().indexOf(employeeInfo);
+        int res = i % rowsPerPage;
+        if(res<0) throw new RuntimeException("page -1 ");
+        return res;
     }
 
     private Node createPage(Integer pageIndex) {
-        System.out.println("pageIndex "+pageIndex);
-        System.out.println("rowsPerPage "+ rowsPerPage);
         int fromIndex = pageIndex * rowsPerPage;
         int toIndex = Math.min(fromIndex+ rowsPerPage, employeesList.size());
-        System.out.println("fromIndex "+fromIndex);
-        System.out.println("toIndex "+toIndex);
-        System.out.println("-+---------------------------------------------");
         employeesTableView.setItems(FXCollections.observableArrayList(employeesList.subList(fromIndex,toIndex)));
         return employeesTableView;
     }
